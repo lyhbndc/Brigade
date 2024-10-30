5cafafd7bbc4cee964f7de92ba5b687c98dc6e5f
lyhbndc <bondoc.aaliyah.b@gmail.com>
2024-10-30 21:46:21 +0800

    ss

--- sample.php
+++ sample.php
@@ -79,6 +79,7 @@
     </div>
   </header>
 
+<<<<<<< Updated upstream
   <main>
 
     <section class="py-5 text-center container">
@@ -91,6 +92,19 @@
             <a href="#" class="btn btn-primary my-2">Main call to action</a>
             <a href="#" class="btn btn-secondary my-2">Secondary action</a>
           </p>
+=======
+<div class="item">
+    <div class="product-gallery">
+        <img id="productImage" src="assets/shirts/Racer/3.png" alt="Product Image">
+        <div class="nav-buttons">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+</svg>
+</svg></button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+</svg></button>
+>>>>>>> Stashed changes
         </div>
       </div>
     </section>
