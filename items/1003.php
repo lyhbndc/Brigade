910b659eb9a774137e104fa3b051a0a1aa634184
lyhbndc <bondoc.aaliyah.b@gmail.com>
2024-11-30 06:08:10 +0800

    item files

--- items/1003.php
+++ items/1003.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Brigade Clothing - Let's Get High</title>
+<title>Brigade Clothing - On the Grind</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -128,7 +128,7 @@
 					<ul>
 						<li><a href="../index.html">Home</a></li>
 						<li><a href="../categories.html"><i class="fa fa-angle-right" aria-hidden="true"></i>Tees</a></li>
-						<li class="active"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Let's Get High</a></li>
+						<li class="active"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>On The Grind</a></li>
 					</ul>
 				</div>
 
@@ -142,14 +142,14 @@
 						<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
 							<div class="single_product_thumbnails">
 								<ul>
-									<li class="active"><img src="images\1001\t1.png" alt="" data-image="images/1001/1.png"></li>
-									<li ><img src="images\1001\t2.png" alt="" data-image="images/1001/2.png"></li>
+									<li class="active"><img src="images\1001\t1.png" alt="" data-image="images/1002/1.png"></li>
+									<li ><img src="images\1001\t2.png" alt="" data-image="images/1002/2.png"></li>
 								</ul>
 							</div>
 						</div>
 						<div class="col-lg-9 image_col order-lg-2 order-1">
 							<div class="single_product_image">
-								<div class="single_product_image_background" style="background-image:url(images/1001/1.png)"></div>
+								<div class="single_product_image_background" style="background-image:url(images/1002/1.png)"></div>
 							</div>
 						</div>
 					</div>
@@ -158,7 +158,7 @@
 			<div class="col-lg-5">
 				<div class="product_details">
 					<div class="product_details_title">
-						<h2>BRIGADE CLOTHING - Let's Get High</h2>
+						<h2>BRIGADE CLOTHING - On The Grind</h2>
 						<p>The Brigade Clothing offers the perfect blend of comfort, style, and bold expression. Designed as a unisex basic T-shirt, this versatile piece is available in sizes ranging from S to XXL, ensuring a fit for everyone. Whether you're lounging or out with friends, this shirt adds an effortlessly cool vibe to your wardrobe.</p>
 					</div>
 					<div class="original_price">₱750.00</div>
@@ -220,12 +220,12 @@
 									<p>Crafted locally with pride, each T-shirt is made from a durable and breathable blend of 80% cotton and 20% polyester. The fabric combination ensures a soft, smooth feel against the skin while providing lasting quality, making it a perfect addition to your everyday essentials.</p>
 								</div>
 								<div class="tab_image">
-									<img src="images\1001\i1.png" alt="">
+									<img src="images\1002\i1.png" alt="">
 								</div>
 							</div>
 							<div class="col-lg-5 offset-lg-2 desc_col">
 								<div class="tab_image">
-									<img src="images\1001\i2.png" alt="">
+									<img src="images\1002\i2.png" alt="">
 								</div>
 								<div class="tab_text_block">
 									<h2>A Statement of Style and Individuality</h2>
@@ -426,16 +426,20 @@
 
 <script>
     const items = [
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "1", name: "Let's Get High" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "2", name: "On The Grind" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "3", name: "Allergic" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "4", name: "Summer Heist" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "5", name: "Nectar" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "6", name: "Bay Area" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "7", name: "Sting" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "8", name: "Daily" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "9", name: "Warm Up" },
- { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "10", name: "Earth" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "1", name: "LETS GET HIGH" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "2", name: "LUCKY BLACK" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "3", name: "CHASE DREAM BLUE" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "4", name: "COLDEST BLUE" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "5", name: "WORD OF KNIVES" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "6", name: "CHASE DREAM WHITE" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "7", name: "MULTIVERSE" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "8", name: "GLOBAL TERROR" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "9", name: "CYBER PUNK" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "10", name: "DAILY" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "11", name: "COOKIES" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "12", name: "WHAT EVER" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "13", name: "YOUR HIRED" },
+ { img: "assets/359801864_251602164294072_4089427261190148458_n.jpg", alt: "14", name: "CHICAGO" },
 ];
 
 const nameList = document.getElementById('nameList');
